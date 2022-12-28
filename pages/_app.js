58f25f972cc5424f74101fa8983d7a49b5b63800
tajender-fyntune <tajendra.batra@fyntune.com>
2022-12-28 18:21:14 +0530

    Title changed

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,23 @@
 import "../styles/globals.css";
 import Navbar from "../components/navbar";
 import { useState } from "react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   const [genre, setGenre] = useState("");
   
   return (
+    <>
+    <Head>
+      <title>Movie Magic</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <div className="bg-black h-full pb-20">
     <div className="container w-11/12 h-full mx-auto">
       <Navbar setGenre={setGenre} />
       <Component genre={genre} {...pageProps} />
       </div>
     </div>
+    </>
   );
 }
